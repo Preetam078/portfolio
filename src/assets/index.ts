6bd990b6a6f6cb3e25bb41fc1e98a3598fb1c8a7
Preetam Mondal <preetammondal78@gmail.com>
2026-06-10 02:29:52 +0530

    added frontend, backend card

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import FrontendTemplate from "../assets/frontend-template.png"
+import BackendTemplate from "../assets/backend-template.png"
+
+const imageMap = {
+    frontendTemplate: FrontendTemplate,
+    backendTemplate: BackendTemplate
+}
+
+export default imageMap;
